3700abfbe8a0d3560d83bc6a3b04a6dbf78d94af
aerofleet <aerofleet72@gmail.com>
2025-04-22 23:24:42 +0900

    favicon update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>itsdev</title>
+        <link rel="icon" type="image/x-icon" href="{{ url('/favicon.ico') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
